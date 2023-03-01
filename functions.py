a684b929438607f421f70052dc7849a5c2f900e6
SirL0fty <sirl0fty@gmail.com>
2023-03-01 18:11:50 +0000

    Update

---
+++ functions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def two_plus_two():
+        val = 2 +2
+        return val
+
+print(two_plus_two() * 2)
+
+def multiply_two( num1, num2 ):
+    val = num1 * num2
+    return val
+
+print(multiply_two( 5, 7))
